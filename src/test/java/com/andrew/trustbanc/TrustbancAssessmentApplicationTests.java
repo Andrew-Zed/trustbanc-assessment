1e340777059bf4e040d6ff700f4da66b2f5476a8
Andrew Awadike <andrew.awadyke@gmail.com>
2023-09-26 17:14:54 +0100

    first commit, Simple CRUD implementation

---
+++ src/test/java/com/andrew/trustbanc/TrustbancAssessmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andrew.trustbanc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrustbancAssessmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
